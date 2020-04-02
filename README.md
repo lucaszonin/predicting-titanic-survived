1e782549878e3fe926abc3fe745d156558bea313
Lucas Zonin <Lucaszsoares1@hotmail.com>
2020-04-01 23:40:53 -0300

    ajustando alguns parametros

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # predicting-titanic-survived
+
+#Required Files
+1 setup.sh
+2 Procfile
+3 requirements.txt
---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import webbrowser
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble.forest import RandomForestClassifier
+from sklearn.feature_selection import SelectFromModel
+
+def main():
+    st.title('Teste hello world')
+    st.subheader('Oi pessoal')
+
+if __name__ == '__main__':
+    main()
