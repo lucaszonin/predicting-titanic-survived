f3943a707dcbe3b6bdcdaa466b24986e8ca59174
Lucas Zonin <Lucaszsoares1@hotmail.com>
2020-04-01 23:58:49 -0300

    alteracao

--- app.py
+++ app.py
@@ -7,8 +7,12 @@ from sklearn.ensemble.forest import RandomForestClassifier
 from sklearn.feature_selection import SelectFromModel
 
 def main():
-    st.title('Teste hello world')
-    st.subheader('Oi pessoal')
+    st.title('Você sobreviveria ao Titanic?')
+    st.write('Modelo de classificação com RandomForest para prever sobrevivência ou morte de passageiros no Titanic')
+    st.write('')
+    if st.button('Linkedin - Lucas Zonin Soares'):
+        webbrowser.open_new_tab('https://www.linkedin.com/in/lucas-z-86a319160/')
+    st.write('')
 
 if __name__ == '__main__':
     main()
